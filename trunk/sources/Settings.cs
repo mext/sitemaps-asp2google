1b4e8a565a1927f09e5d26e02b89dd0f44e221dc
erymski <erymski@80884ddb-011b-0410-9d15-0de0643b89d2>
2006-11-02 20:35:31 +0000

    More proper way to check if it's a first start.

--- trunk/sources/Settings.cs
+++ trunk/sources/Settings.cs
@@ -1,42 +1,21 @@
-namespace SitemapConverter.Properties {
-    
-    
-    // This class allows you to handle specific events on the settings class:
-    //  The SettingChanging event is raised before a setting's value is changed.
-    //  The PropertyChanged event is raised after a setting's value is changed.
-    //  The SettingsLoaded event is raised after the setting values are loaded.
-    //  The SettingsSaving event is raised before the setting values are saved.
-    internal sealed partial class Settings {
-        
-        public Settings() {
-            // // To add event handlers for saving and changing settings, uncomment the lines below:
-            //
-            // this.SettingChanging += this.SettingChangingEventHandler;
-            //
-            // this.SettingsSaving += this.SettingsSavingEventHandler;
-            //
+using System.Configuration;
 
-            this.SettingsLoaded += new System.Configuration.SettingsLoadedEventHandler(Settings_SettingsLoaded);
+namespace SitemapConverter.Properties 
+{
+    internal sealed partial class Settings 
+    {
+        public Settings() 
+        {
+            SettingsLoaded += new SettingsLoadedEventHandler(Settings_SettingsLoaded);
         }
 
-        void Settings_SettingsLoaded(object sender, System.Configuration.SettingsLoadedEventArgs e)
+        void Settings_SettingsLoaded(object sender, SettingsLoadedEventArgs e)
         {
-            // if all properties are empty - try to read 
-            // them from previous version of the program
-            if (string.IsNullOrEmpty(AspNetSitemap) &&
-                string.IsNullOrEmpty(DomainName) &&
-                string.IsNullOrEmpty(GoogleSitemap))
+            if (IsFirstRun)
             {
                 Upgrade();
+                IsFirstRun = false;
             }
         }
-        
-        //private void SettingChangingEventHandler(object sender, System.Configuration.SettingChangingEventArgs e) {
-        //    // Add code to handle the SettingChangingEvent event here.
-        //}
-        
-        //private void SettingsSavingEventHandler(object sender, System.ComponentModel.CancelEventArgs e) {
-        //    // Add code to handle the SettingsSaving event here.
-        //}
     }
 }
